d8f4a33c56fffec14c3264e68ae5108c4e5fb1ad
CorentinDWWM <newsonic62@gmail.com>
2025-05-26 10:40:44 +0200

    push

--- back/index.js
+++ back/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const app = express();
-
-app.use(cors({ origin: "*", credentials: true }));
-
-app.use(express.json());
-
+const path = require("path");
 const postRoutes = require("./routes/post");
+const app = express();
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+const __DIRNAME = path.resolve();
+
+app.use(express.static(path.join(__DIRNAME, "/front/dist")));
 
 app.use("/post", postRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__DIRNAME, "front", "dist", "index.html"));
+});
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("✅ MongoDB connected");
